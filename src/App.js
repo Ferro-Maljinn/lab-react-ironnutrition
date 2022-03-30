467cf15d2d243c6bcb4ddb7c1061892f25e5a755
Adalia da Silva <ada.future.work@gmail.com>
2022-03-30 16:04:24 +0200

    iteration 3

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
-
-import 'antd/dist/antd.css'
+import 'antd/dist/antd.css';
 import './App.css';
 import { Card, Row, Col, Divider, Input, Button } from 'antd';
 import foods from './foods.json';
 import {useState} from 'react';
 import { FoodBox } from './components/FoodBox';
 
+
 function App() {
-
   const [ allFoods, setAllFoods ] = useState(foods)
-
   return (
-    <div className="App">
-      {allFoods.map((foods) => {
+    <div className='App'>
+  <FoodBox food={ {
+            name: 'Orange',
+            calories: 85,
+            image: 'https://i.imgur.com/abKGOcv.jpg',
+            servings: 1
+}} />
+      {
+       allFoods.map((element) => {
         return (
-          <div>
-            <h2> {foods.name} </h2>
-            <img src={foods.image} alt="food" width={100} />
-          </div>
-        );
-      })}
-      
-      <FoodBox
-        food={{
-          name: 'Orange',
-          calories: 85,
-          image: 'https://i.imgur.com/abKGOcv.jpg',
-          servings: 1,
-        }}
-      />
+          <FoodBox food={ element } />
+          )
+        })
+      }
     </div>
   );
 }
--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -1,24 +1,21 @@
 import { Card, Row, Col, Divider, Input, Button } from 'antd';
 import React from 'react';
 
-export function FoodBox(props) {
-  return (
-    <Col>
-      <Card
-        title={props.food.name}
-        style={{ width: 230, height: 300, margin: 10 }}
-      >
-        <img src={props.food.image} height={60} alt="food" />
-        <p>Calories: {props.food.calories} </p>
-        <p>Servings: {props.food.servings}</p>
-        <p>
-          <b>
-            {/* Total Calories: {´${props.food.calories} * ${props.food.servings}´} */}kcal
-          </b>
-          
-        </p>
-        <Button type="primary"> Delete </Button>
-      </Card>
-    </Col>
-  );
+export function FoodBox(props){
+    return (
+        <Col>
+          <Card
+            title={props.food.name}
+            style={{ width: 230, height: 300, margin: 10 }}
+          >
+            <img src={props.food.image} height={60} alt='food' />
+            <p>Calories: {props.food.calories}</p>
+            <p>Servings: {props.food.servings}</p>
+            <p>
+              <b>Total Calories: FOOD_CALORIES * FOOD_SERVINGS </b> kcal
+            </p>
+            <Button type='primary'> Delete </Button>
+          </Card>
+        </Col>
+      );
 }
